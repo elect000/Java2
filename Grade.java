16bd26db546e763696d620b8a8dbdec25c2130b0
elect <e.tmailbank@gmail.com>
2016-07-12 16:19:29 +0900

    re

---
+++ Grade.java
@@ -0,0 +1,17 @@
+public class Grade {
+    public static void main(String[] args) throws Exception {
+	BufferedReader br =
+	    new BufferedReader(new InputStreamReader(Sytem.in));
+	String str = br.readLine();
+	int res = Integer.parseInt(str);
+	if(res>=50){
+	    System.out.println("A");
+	}
+	else if(res<50){
+	    System.out.println("B");
+	}
+	else{
+	    System.out.println("error");
+	}
+    }
+}
